6a2c25a07fccbedc94ebfa55225a9c7b084653f7
redradist <redradist@gmail.com>
2022-05-02 22:04:55 +0300

    Program 2.3 Modules to Catch GLSL Errors

--- main.cpp
+++ main.cpp
@@ -10,7 +10,47 @@ using namespace std;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
+void printShaderLog(GLuint shader) {
+  int len = 0;
+  int chWrittn = 0;
+  char *log;
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+  if (len > 0) {
+    log = (char *)malloc(len);
+    glGetShaderInfoLog(shader, len, &chWrittn, log);
+    cout << "Shader Info Log: " << log << endl;
+    free(log);
+  }
+}
+
+void printProgramLog(int prog) {
+  int len = 0;
+  int chWrittn = 0;
+  char *log;
+  glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+  if (len > 0) {
+    log = (char *)malloc(len);
+    glGetProgramInfoLog(prog, len, &chWrittn, log);
+    cout << "Program Info Log: " << log << endl;
+    free(log);
+  }
+}
+
+bool checkOpenGLError() {
+  bool foundError = false;
+  int glErr = glGetError();
+  while (glErr != GL_NO_ERROR) {
+    cout << "glError: " << glErr << endl;
+    foundError = true;
+    glErr = glGetError();
+  }
+  return foundError;
+}
+
 GLuint createShaderProgram() {
+  GLint vertCompiled;
+  GLint fragCompiled;
+  GLint linked;
   const char *vshaderSource =
       "#version 430 \n"
       "void main(void) \n"
@@ -25,11 +65,29 @@ GLuint createShaderProgram() {
   glShaderSource(vShader, 1, &vshaderSource, NULL);
   glShaderSource(fShader, 1, &fshaderSource, NULL);
   glCompileShader(vShader);
+  checkOpenGLError();
+  glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+  if (vertCompiled != 1) {
+    cout << "vertex compilation failed" << endl;
+    printShaderLog(vShader);
+  }
   glCompileShader(fShader);
+  checkOpenGLError();
+  glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+  if (fragCompiled != 1) {
+    cout << "fragment compilation failed" << endl;
+    printShaderLog(fShader);
+  }
   GLuint vfProgram = glCreateProgram();
   glAttachShader(vfProgram, vShader);
   glAttachShader(vfProgram, fShader);
   glLinkProgram(vfProgram);
+  checkOpenGLError();
+  glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+  if (linked != 1) {
+    cout << "linking failed" << endl;
+    printProgramLog(vfProgram);
+  }
   return vfProgram;
 }
 
