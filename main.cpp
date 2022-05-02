e643d61640ad64cc13a27936b2b459414f6c7987
redradist <redradist@gmail.com>
2022-05-02 13:12:40 +0300

    Program 2.2 Shaders, Drawing a POINT

--- main.cpp
+++ main.cpp
@@ -4,14 +4,46 @@
 
 using namespace std;
 
-void init(GLFWwindow* window) { }
+#define numVAOs 1
 
-void display(GLFWwindow* window, double currentTime) {
-  glClearColor(1.0, 0.0, 0.0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT);
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+  const char *vshaderSource =
+      "#version 430 \n"
+      "void main(void) \n"
+      "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+  const char *fshaderSource =
+      "#version 430 \n"
+      "out vec4 color; \n"
+      "void main(void) \n"
+      "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+  GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+  GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(vShader, 1, &vshaderSource, NULL);
+  glShaderSource(fShader, 1, &fshaderSource, NULL);
+  glCompileShader(vShader);
+  glCompileShader(fShader);
+  GLuint vfProgram = glCreateProgram();
+  glAttachShader(vfProgram, vShader);
+  glAttachShader(vfProgram, fShader);
+  glLinkProgram(vfProgram);
+  return vfProgram;
 }
 
-int main(void) {
+void init(GLFWwindow* window) {
+  renderingProgram = createShaderProgram();
+  glGenVertexArrays(numVAOs, vao);
+  glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+  glUseProgram(renderingProgram);
+  glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main() {
   if (!glfwInit()) { exit(EXIT_FAILURE); }
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
