6ff9e723300a1b0737c69c890b6947b1d6e5fcca
redradist <redradist@gmail.com>
2022-05-16 00:13:15 +0300

    Program 2.4 Reading GLSL Source from Files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "opengl/opengl.hpp"
 #include "opengl/log.hpp"
 
@@ -12,21 +14,29 @@ using namespace std;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
+std::string readShaderSource(const char *filePath) {
+  std::string content;
+  std::ifstream fileStream(filePath, ios::in);
+  std::string line = "";
+  while (!fileStream.eof()) {
+    std::getline(fileStream, line);
+    content.append(line + "\n");
+  }
+  fileStream.close();
+  return content;
+}
+
 GLuint createShaderProgram() {
   GLint vertCompiled;
   GLint fragCompiled;
   GLint linked;
-  const char *vshaderSource =
-      "#version 430 \n"
-      "void main(void) \n"
-      "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
-  const char *fshaderSource =
-      "#version 430 \n"
-      "out vec4 color; \n"
-      "void main(void) \n"
-      "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
   GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
   GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+  string vertShaderStr = readShaderSource("../../shaders/vertex.glsl");
+  string fragShaderStr = readShaderSource("../../shaders/fragment.glsl");
+  const char *vshaderSource = vertShaderStr.c_str();
+  const char *fshaderSource = fragShaderStr.c_str();
   glShaderSource(vShader, 1, &vshaderSource, NULL);
   glShaderSource(fShader, 1, &fshaderSource, NULL);
   glCompileShader(vShader);
